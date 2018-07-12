36e25c72d16d5eda9e79bdb88473e396db86306b
Ryan Gregg <ryangregg@google.com>
2018-07-11 23:43:51 -0700

    Initial commit for the app

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	// Imports the Google Cloud Datastore client package
+	"cloud.google.com/go/datastore"
+	"golang.org/x/net/context"
+
+	// Imports UUID generator
+	"github.com/satori/go.uuid"
+)
+
+const ()
+
+var (
+	projectID = os.Getenv("PROJECT_ID")
+	ctx       context.Context
+	client    *datastore.Client
+)
+
+type GuestEntry struct {
+	Message    string
+	PostedTime time.Time
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	fmt.Fprintln(w, "Hello, Gopher Network")
+}
+
+func writeEntryHandler(w http.ResponseWriter, r *http.Request) {
+	// Write a new entry into the guest log
+
+	kind := "GuestEntry"
+
+	id, _ := uuid.NewV4()
+	name := id.String()
+	entryKey := datastore.NameKey(kind, name, nil)
+
+	entry := GuestEntry{
+		Message:    r.URL.Query().Get("message"),
+		PostedTime: time.Now()}
+
+	if _, err := client.Put(ctx, entryKey, &entry); err != nil {
+		log.Printf("Failed to save the entry: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to save the entry"))
+		return
+	}
+
+	log.Printf("Saved %v: %v\n", entryKey, entry.Message)
+	w.Write([]byte(fmt.Sprintf("Saved %v: %v\n", name, entry.Message)))
+}
+
+func listEntriesHandler(w http.ResponseWriter, r *http.Request) {
+	var entities []GuestEntry
+	q := datastore.NewQuery("GuestEntry").Order("-PostedTime").Limit(20)
+	_, err := client.GetAll(ctx, q, &entities)
+	if err != nil {
+		log.Printf("Failed to retrieve entries: %v\n", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json, err := json.Marshal(entities)
+	fmt.Fprintf(w, "%s", json)
+}
+
+func main() {
+
+	// Initialize data store
+	ctx = context.Background()
+
+	// Create the datastore client
+	client, _ = datastore.NewClient(ctx, projectID)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/post", writeEntryHandler)
+	http.HandleFunc("/list", listEntriesHandler)
+	// http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
